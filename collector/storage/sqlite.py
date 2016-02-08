d77d38d1accd6b6c89a8c8226bc01d6357f64aeb
Luis Capelo <luiscape@gmail.com>
2016-02-08 17:54:52 -0500

    Adding SQLite storage.

---
+++ collector/storage/sqlite.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+SQLITE:
+-------
+
+Stores Python dictionaries as SQLite
+records.
+
+'''
+import scraperwiki
+
+from collector.utilities.item import item
+
+def store_sqlite(data, table):
+  '''
+  Store records in a SQLite database.
+
+  '''
+  print('{bullet} Storing {n} records in database.'.format(bullet=item('bullet'), n=len(data)))
+
+  for record in data:
+    scraperwiki.sqlite.save(record.keys(), record, table_name=table)
