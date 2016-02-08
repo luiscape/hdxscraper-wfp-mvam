c9c3d76fa5acea7de72ed4ad73646b07842a4a96
Luis Capelo <luiscape@gmail.com>
2016-02-08 17:54:12 -0500

    Storing results in SQLite.

--- collect.py
+++ collect.py
@@ -8,21 +8,31 @@ Here we query the WFP API and collect all available
 data, storing the results in local CSV files.
 
 '''
+import scraperwiki
+
 from collector.classes.mvam import mVAM
 from collector.utilities.item import item
 from collector.storage.csv import store_csv
+from collector.storage.sqlite import store_sqlite
 
 def main():
     '''
     Program wrapper.
 
     '''
-    m = mVAM(table='pblStatsSum')
-    store_csv(data=m.query(), path='pblStatsSum.csv')
-
-    m = mVAM(table='pblStatsSum4Maps')
-    store_csv(data=m.query(), path='pblStatsSum4Maps.csv')
+    tables = ['pblStatsSum', 'pblStatsSum4Maps']
+    for t in tables:
+        m = mVAM(table=t)
+        records = m.query()
+        store_csv(data=records, path='%s.csv' % t)
+        store_sqlite(data=records, table=t)
 
 if __name__ == '__main__':
+  try:
     main()
     print('%s Successfully collected mVAM data.' % item('success'))
+    scraperwiki.status('ok')
+
+  except Exception as e:
+    print('%s Failed to collected mVAM data.' % item('error'))
+    scraperwiki.status('error', 'Failed to collect data.')
